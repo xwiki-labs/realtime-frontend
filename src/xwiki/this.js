4ab9d8c532d9356adfe85d1d56b960f637986778
yflory <yann.flory@xwiki.com>
2016-07-06 17:43:38 +0200

    Update version to 1.5

--- src/xwiki/this.js
+++ src/xwiki/this.js
@@ -2,5 +2,5 @@ Package(function (pack, XWiki) {
     pack.setName("Realtime Editor Frontend");
     pack.setDescription("Collaboratively edit your XWiki Documents with others");
     pack.setExtensionId("org.xwiki.contrib:xwiki-contrib-rtfrontend");
-    pack.setXarName("realtime-frontend-1.4.xar")
+    pack.setXarName("realtime-frontend-1.5.xar")
 });
