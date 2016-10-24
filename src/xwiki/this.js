f6b1db80b75dff9c491ffe4651197faba9a6bc52
yflory <yann.flory@xwiki.com>
2016-10-24 14:14:11 +0200

    Set the new version

--- src/xwiki/this.js
+++ src/xwiki/this.js
@@ -2,5 +2,5 @@ Package(function (pack, XWiki) {
     pack.setName("Realtime Editor Frontend");
     pack.setDescription("Collaboratively edit your XWiki Documents with others");
     pack.setExtensionId("org.xwiki.contrib:xwiki-contrib-rtfrontend");
-    pack.setXarName("realtime-frontend-1.8.xar")
+    pack.setXarName("realtime-frontend-1.9.xar")
 });
