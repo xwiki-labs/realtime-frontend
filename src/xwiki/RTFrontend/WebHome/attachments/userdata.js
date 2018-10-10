ac9c802121f2ed15e366d6876a4bda859baf91b0
yflory <yann.flory@xwiki.com>
2018-10-10 18:51:46 +0200

    Fix reconnect issues

--- src/xwiki/RTFrontend/WebHome/attachments/userdata.js
+++ src/xwiki/RTFrontend/WebHome/attachments/userdata.js
@@ -26,6 +26,7 @@ define(['RTFrontend_realtime_input',
         var userAvatar = configData.userAvatar;
         if (!myId || !userName) { warn("myId and userName are required!"); return; }
 
+        var online = true;
         var cursor;
         var editor = configData.editor;
         var oldcursor;
@@ -56,6 +57,7 @@ define(['RTFrontend_realtime_input',
                 }
                 if (onChange && typeof onChange === "function") { onChange(userData); }
             } catch (e) {
+                console.log(textData);
                 console.error(e);
             }
         };
@@ -66,9 +68,20 @@ define(['RTFrontend_realtime_input',
 
             updateUserData(userDoc);
         };
+
+        var first = true;
         var onReady = config.onReady =  function(info) {
-            var realtime = module.realtime = info.realtime;
             module.leave = info.leave;
+            var realtime = module.realtime = info.realtime;
+            if (!first) {
+                var userDoc = realtime.getUserDoc();
+                updateUserData(userDoc);
+                initializing = false;
+                onLocal();
+                return;
+            }
+
+            first = false;
             module.patchText = TextPatcher.create({
                 realtime : realtime,
                 logging : false
@@ -82,6 +95,7 @@ define(['RTFrontend_realtime_input',
         };
         var onLocal = config.onLocal = function() {
             if (initializing) { return; }
+            if (!online) { return; }
 
             var shjson = stringify(userData);
             module.patchText(shjson);
@@ -89,12 +103,26 @@ define(['RTFrontend_realtime_input',
                 warn("userDoc !== shjson");
             }
         };
+        config.onConnectionChange = function (info) {
+            if (info.state) {
+                myId = info.myId
+                online = true;
+                initializing = true;
+                return;
+            }
+            online = false;
+        };
 
         var to;
         if (typeof cursor !== "undefined") {
             to = setInterval(function () {
+                if (!online) { return; }
                 var newcursor = cursor();
                 if (oldcursor !== newcursor) {
+                    if (!userData[myId]) {
+                        userData[myId] = { name : userName };
+                        if (typeof userAvatar === "string") { userData[myId].avatar = userAvatar; }
+                    }
                     userData[myId]['cursor_'+editor] = newcursor;
                     oldcursor = newcursor;
                     onChange(userData);
